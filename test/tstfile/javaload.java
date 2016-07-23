2059ab38db887b85a3d3c04ef71814bc1a146691
jeppeter <jeppeter@gmail.com>
2016-07-23 18:55:53 +0800

    to add javaload

---
+++ test/tstfile/javaload.java
@@ -0,0 +1,30 @@
+
+import java.lang.reflect.*;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Subparsers;
+import net.sourceforge.argparse4j.inf.Subparser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+
+class LoadMethodClass extends ClassLoader {
+	public LoadMethodClass() {
+		super();
+	}
+
+	private Class<?> __getClass(String clsname) {
+
+	}
+
+	public Method getClassMethod(String clsmeth) {
+
+	}
+}
+
+public class javaload {
+	public static void main(String... args) {
+
+	}
+}
