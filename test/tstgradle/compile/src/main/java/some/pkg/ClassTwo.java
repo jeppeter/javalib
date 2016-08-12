2323098c67608bc9967c4a476038f71ab23d5616
jeppeter <jeppeter@gmail.com>
2016-08-13 06:33:35 +0800

    to make runOne

--- test/tstgradle/compile/src/main/java/some/pkg/ClassTwo.java
+++ test/tstgradle/compile/src/main/java/some/pkg/ClassTwo.java
@@ -7,4 +7,8 @@ public class ClassTwo {
         ClassOne cone = new ClassOne();
         cone.doStuff();
     }
+
+    public static void main(String... args) {
+    	System.out.println("hello world");
+    }
 }
