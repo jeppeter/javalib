490845c4634d708606186592568a697d6cedaf23
jeppeter <jeppeter@gmail.com>
2016-10-06 11:52:04 +0800

    to make compiling error

--- extargsparse4j/src/main/java/com/github/jeppeter/extargsparse4j/Key.java
+++ extargsparse4j/src/main/java/com/github/jeppeter/extargsparse4j/Key.java
@@ -3,11 +3,16 @@ package com.github.jeppeter.extargsparse4j;
 
 import java.util.regex.*;
 import com.github.jeppeter.extargsparse4j.KeyException;
-import reext.ReExt;
+import com.github.jeppeter.reext.ReExt;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import com.github.jeppeter.jsonext.JsonExt;
 import java.lang.reflect.Field;
+import java.util.regex.*;
+import java.util.Arrays;
+import java.lang.Long;
+import com.github.jeppeter.jsonext.JsonExtInvalidTypeException;
+import com.github.jeppeter.jsonext.JsonExtNotParsedException;
 
 class TypeClass {
 	private String m_typename;
@@ -55,12 +60,12 @@ public final class Key {
 	private boolean m_iscmd;
 	private boolean m_isflag;
 	private String m_type;
-	private Pattern m_helpexpr;
-	private Pattern m_cmdexpr;
-	private Pattern m_prefixexpr;
-	private Pattern m_funcexpr;
-	private Patern m_flagexpr;
-	private Pattern m_mustflagexpr;
+	private ReExt m_helpexpr;
+	private ReExt m_cmdexpr;
+	private ReExt m_prefixexpr;
+	private ReExt m_funcexpr;
+	private ReExt m_flagexpr;
+	private ReExt m_mustflagexpr;
 
 	private void __reset() {
 		this.m_value = null;
@@ -79,6 +84,7 @@ public final class Key {
 	}
 
 	private void __validate()  throws KeyException {
+		TypeClass typecls;
 		if (this.m_isflag) {
 			assert(!this.m_iscmd);
 			if (this.m_function != null ) {
@@ -89,7 +95,8 @@ public final class Key {
 				throw new KeyException(String.format("(%s) flag can not accept dict", this.m_origkey));
 			}
 
-			if (this.m_type != TypeClass(this.m_value).get_type() && this.m_type != "count") {
+			typecls = new TypeClass(this.m_value);
+			if (this.m_type != typecls.get_type() && this.m_type != "count") {
 				throw new KeyException(String.format("(%s) not match type(%s)", this.m_origkey, this.m_type));
 			}
 
@@ -98,7 +105,8 @@ public final class Key {
 					throw new KeyException(String.format("(%s) should at least for prefix", this.m_origkey));
 				}
 				this.m_type = "prefix";
-				if (TypeClass(this.m_value).get_type() != "dict") {
+				typecls = new TypeClass(this.m_value);
+				if (typecls.get_type() != "dict") {
 					throw new KeyException(String.format("(%s) should make dict for prefix", this.m_origkey));
 				}
 
@@ -114,13 +122,13 @@ public final class Key {
 					throw new KeyException(String.format("(%s) should not has shortflag", this.m_origkey));
 				}
 			} else {
-				if (this.m_flagname.length <= 0) {
+				if (this.m_flagname.length() <= 0) {
 					throw new KeyException(String.format("(%s) flagname <= 0", this.m_origkey));
 				}
 			}
 
 			if (this.m_shortflag != null ) {
-				if (this.m_shortflag.length > 1) {
+				if (this.m_shortflag.length() > 1) {
 					throw new KeyException(String.format("(%s) shortflag > 1", this.m_origkey));
 				}
 			}
@@ -141,7 +149,7 @@ public final class Key {
 				}
 			}
 		} else {
-			if (this.m_cmdname == null || this.m_cmdname.length == 0) {
+			if (this.m_cmdname == null || this.m_cmdname.length() == 0) {
 				throw new KeyException(String.format("(%s) no cmdnname", this.m_origkey));
 			}
 			if (this.m_shortflag != null) {
@@ -161,22 +169,23 @@ public final class Key {
 		return;
 	}
 
-	private String __get_m_field_string(String fldname) {
+	private String __get_m_field_string(String fldname) throws NoSuchFieldException,IllegalAccessException {
 		Field fld;
 		String innername = String.format("m_%s", fldname);
 		fld = this.getClass().getDeclaredField(innername);
 		return (String) fld.get((Object) this);
 	}
 
-	private Boolean __get_m_field_bool(String fldname) {
+	private Boolean __get_m_field_bool(String fldname) throws NoSuchFieldException,IllegalAccessException {
 		Field fld;
 		String innername = String.format("m_%s",fldname);
 		fld = this.getClass().getDeclaredField(innername);
 		return (Boolean) fld.get((Object)this);
 	}
 
-	private void __set_m_field_string(String fldname,String value) {
+	private void __set_m_field_string(String fldname,String value) throws NoSuchFieldException,IllegalAccessException {
 		String innername = String.format("m_%s", fldname);
+		Field fld;
 		fld = this.getClass().getDeclaredField(innername);
 		fld.set((Object) this,(Object)value);
 		return;
@@ -184,17 +193,18 @@ public final class Key {
 
 
 
-	private void __set_flag(String prefix, String key, Object value) {
+	private void __set_flag(String prefix, String key, Object value) throws JsonExtInvalidTypeException,JsonExtNotParsedException {
 		String[] keys;
 		JsonExt json = new JsonExt();
 		String sobj;
 		int i;
+		TypeClass typecls;
 		this.m_isflag = true;
 		this.m_iscmd = false;
 		this.m_origkey = key;
 		json.Clone(value);
 		keys = json.getKeys("/");
-		if (keys == null ||  !Array.asList(keys).contains("value")) {
+		if (keys == null ||  !Arrays.asList(keys).contains("value")) {
 			this.m_value = null;
 			this.m_type = "string";
 		}
@@ -210,21 +220,23 @@ public final class Key {
 				} else if (Arrays.asList(this.m_flagspecial).contains(keys[i])) {
 					String newprefix;
 					if (keys[i] == "prefix") {
-						if (TypeClass(json.getObject(keys[i])).get_type() != "string") {
-							throw new KeyException(String.format("(%s).prefix type %s", this.m_origkey, TypeClass(json.getObject(keys[i])).get_type()));
+						typecls = new TypeClass(json.getObject(keys[i]));
+						if (typecls.get_type() != "string") {
+							throw new KeyException(String.format("(%s).prefix type %s", this.m_origkey, typecls.get_type()));
 						}
 						newprefix = "";
-						if (prefix != null && prefix.length > 0) {
+						if (prefix != null && prefix.length() > 0) {
 							newprefix += prefix;
 						}
 						newprefix += json.getString(keys[i]);
 						this.m_prefix = newprefix;
 					} else if (keys[i] == "value") {
-						if (TypeClass(json.getObject(keys[i])).get_type() == "dict") {
+						typecls = new TypeClass(json.getObject(keys[i]));
+						if (typecls.get_type() == "dict") {
 							throw new KeyException(String.format("(%s) %s should not be dict", this.m_origkey, keys[i]));
 						}
 						this.m_value = value;
-						this.m_type = TypeClass(json.getObject(keys[i])).get_type();
+						this.m_type = typecls.get_type();
 					} else {
 						throw new KeyException(String.format("(%s).%s not valid",this.m_origkey,keys[i]));
 					}
@@ -232,7 +244,7 @@ public final class Key {
 			}
 		}
 
-		if ((this.m_prefix == null || this.m_prefix.length == 0) && (prefix != null && prefix.length > 0)) {
+		if ((this.m_prefix == null || this.m_prefix.length() == 0) && (prefix != null && prefix.length() > 0)) {
 			this.m_prefix = prefix;
 		}
 		return ;
@@ -250,15 +262,17 @@ public final class Key {
 		String cmd ;
 		String[] retstr;
 		String newprefix;
+		String sobj;
 		boolean valid;
+		TypeClass typecls;
 		this.m_origkey = key;
 		if (this.m_origkey.contains("$")) {
-			if (this.m_origkey.charAt(0) != "$") {
-				throw new KeyException(String.Format("can not accept key(%s)", this.m_origkey));
+			if (this.m_origkey.charAt(0) != '$') {
+				throw new KeyException(String.format("can not accept key(%s)", this.m_origkey));
 			}
-			for (i = 1; i < this.m_origkey.length; i++) {
-				if (this.m_origkey.charAt(i) == "$") {
-					throw new KeyException(String.Format("(%s) has ($) more than one", this.m_origkey));
+			for (i = 1; i < this.m_origkey.length(); i++) {
+				if (this.m_origkey.charAt(i) == '$') {
+					throw new KeyException(String.format("(%s) has ($) more than one", this.m_origkey));
 				}
 			}
 		}
@@ -271,45 +285,45 @@ public final class Key {
 				flags = this.m_mustflagexpr.getMatch(0, 0);
 			}
 
-			if (flags == null && this.m_origkey.charAt(0) == "$") {
+			if (flags == null && this.m_origkey.charAt(0) == '$') {
 				this.m_flagname = "$";
-				flagmod = True;
+				flagmode = true;
 			}
 
 			if (flags != null) {
 				if (flags.contains("|")) {
-					retstr = ReExt.Split("\|", flags);
-					if (retstr.length > 2 || ( retstr[1].length != 1) || ( retstr[0].length <= 1)) {
-						throw new KeyException(String.Format("(%s) (%s)flag only accept (longop|l) format", this.m_origkey, flags));
+					retstr = ReExt.Split("\\|", flags);
+					if (retstr.length > 2 || ( retstr[1].length() != 1) || ( retstr[0].length() <= 1)) {
+						throw new KeyException(String.format("(%s) (%s)flag only accept (longop|l) format", this.m_origkey, flags));
 					}
 					this.m_flagname = retstr[0];
 					this.m_shortflag = retstr[1];
 				} else {
 					this.m_flagname = flags ;
 				}
-				flagmod = True;
+				flagmode = true;
 			}
 		} else {
 			this.m_mustflagexpr.FindAll(this.m_origkey);
 			flags = this.m_mustflagexpr.getMatch(0, 0);
 			if (flags != null) {
 				if (flags.contains("|")) {
-					retstr = ReExt.Split("\|", flags);
-					if (retstr.length > 2 || (retstr[1].length != 1  ) || (retstr[0].length <= 1 )) {
-						throw new KeyException(String.Format("(%s) (%s)flag only accept (longop|l) format", this.m_origkey, flags));
+					retstr = ReExt.Split("\\|", flags);
+					if (retstr.length > 2 || (retstr[1].length() != 1  ) || (retstr[0].length() <= 1 )) {
+						throw new KeyException(String.format("(%s) (%s)flag only accept (longop|l) format", this.m_origkey, flags));
 					}
 					this.m_flagname = retstr[0];
 					this.m_shortflag = retstr[1];
 				} else {
-					if (flags.length <= 1) {
-						throw new KeyException(String.Format("(%s) flag must have longopt", this.m_origkey));
+					if (flags.length() <= 1) {
+						throw new KeyException(String.format("(%s) flag must have longopt", this.m_origkey));
 					}
 					this.m_flagname = flags;
 				}
-				flagmode = True;
-			} else if (this.m_origkey[0] == "$") {
+				flagmode = true;
+			} else if (this.m_origkey.charAt(0) == '$') {
 				this.m_flagname  = "$";
-				flagmode = True;
+				flagmode = true;
 			}
 			this.m_cmdexpr.FindAll(this.m_origkey);
 			cmd = this.m_cmdexpr.getMatch(0, 0);
@@ -318,19 +332,19 @@ public final class Key {
 				if (cmd.contains("|"))  {
 					flags = cmd;
 					if (flags.contains("|")) {
-						retstr = ReExt.Split("\|", flags);
-						if (retstr.length > 2 || retstr[1].length != 1 || retstr[0].length <= 1) {
-							throw new KeyException(String.Format('(%s) (%s)flag only accept (longop|l) format', self.__origkey, flags));
+						retstr = ReExt.Split("\\|", flags);
+						if (retstr.length > 2 || retstr[1].length() != 1 || retstr[0].length() <= 1) {
+							throw new KeyException(String.format("(%s) (%s)flag only accept (longop|l) format", this.m_origkey, flags));
 						}
 						this.m_flagname = retstr[0];
 						this.m_shortflag = retstr[1];
 					} else {
 						assert(false);
 					}
-					flagmode = True;
+					flagmode = true;
 				} else {
 					this.m_cmdname = cmd;
-					cmdmode = True;
+					cmdmode = true;
 				}
 			}
 		}
@@ -347,7 +361,7 @@ public final class Key {
 			this.m_helpinfo = mstr;
 		}
 		newprefix = "";
-		if (prefix != null && prefix.length > 0) {
+		if (prefix != null && prefix.length() > 0) {
 			newprefix = String.format("%s_", prefix);
 		}
 
@@ -357,34 +371,35 @@ public final class Key {
 			newprefix += mstr;
 			this.m_prefix = newprefix;
 		} else {
-			if (prefix.length > 0) {
+			if (prefix.length() > 0) {
 				this.m_prefix = prefix;
 			}
 		}
 
 		if (flagmode) {
-			this.m_isflag = True;
-			this.m_iscmd = False;
+			this.m_isflag = true;
+			this.m_iscmd = false;
 		}
 
 		if (cmdmode) {
-			this.m_iscmd = True;
-			this.m_isflag = False;
+			this.m_iscmd = true;
+			this.m_isflag = false;
 		}
 
 		if (!flagmode && !cmdmode) {
-			this.m_isflag = True;
-			this.m_iscmd = False;
+			this.m_isflag = true;
+			this.m_iscmd = false;
 		}
 
 		this.m_value = value;
-		this.m_type = TypeClass(value).get_type();
+		typecls = new TypeClass(value);
+		this.m_type = typecls.get_type();
 
 		if (cmdmode && this.m_type != "dict") {
-			flagmode = True;
-			cmdmode = False;
-			this.m_isflag = True;
-			this.m_iscmd = False;
+			flagmode = true;
+			cmdmode = false;
+			this.m_isflag = true;
+			this.m_iscmd = false;
 			this.m_flagname = this.m_cmdname;
 			this.m_cmdname = null;
 		}
@@ -392,7 +407,7 @@ public final class Key {
 		if (this.m_isflag && this.m_type == "string"  ) {
 			sobj = (String) this.m_value;
 			if (sobj == "+" && this.m_flagname != "$") {
-				lobj = Long(0);
+				lobj = Long.decode(String.format("0"));
 				this.m_value = (Object) lobj;
 				this.m_type = "count";
 				this.m_nargs = "0";
@@ -400,20 +415,20 @@ public final class Key {
 		}
 
 		if (this.m_isflag && this.m_flagname == "$" && this.m_type != "dict") {
-			valid = False;
+			valid = false;
 			if (this.m_type == "string") {
 				sobj = (String) this.m_value;
 				if (sobj == "+" || 
 					sobj == "?" ||
 					sobj == "*") {
-					valid = True;
+					valid = true;
 				}
 				this.m_nargs = sobj;
 				this.m_type = "count";
 				this.m_value = null;
 
 			} else if (this.m_type == "long") {
-				valid = True;
+				valid = true;
 				lobj = (Long) this.m_value;
 				this.m_nargs = String.format("%d",lobj);
 				this.m_type = "count";
@@ -433,13 +448,13 @@ public final class Key {
 	}
 
 	protected Key(String prefix, String key, Object value, boolean isflag) {
-		this.reset();
-		this.m_helpexpr = new ReExt("##([^#]+)##$", True);
-		this.m_cmdexpr = new ReExt("^([^\#\<\>\+\$]+)", True);
-		this.m_prefixexpr = new ReExt("\+([^\+\#\<\>\|\$ \t]+)", True);
-		this.m_funcexpr = new ReExt("<([^\<\>\#\$\| \t]+)>", True);
-		this.m_flagexpr = new ReExt("^([^\<\>\#\+\$ \t]+)", True);
-		this.m_mustflagexpr = new ReExt("^\$([^\$\+\#\<\>]+)", True);
+		this.__reset();
+		this.m_helpexpr = new ReExt("##([^#]+)##$", true);
+		this.m_cmdexpr = new ReExt("^([^\\#\\<\\>\\+\\$]+)", true);
+		this.m_prefixexpr = new ReExt("\\+([^\\+\\#\\<\\>\\|\\$ \\t]+)", true);
+		this.m_funcexpr = new ReExt("<([^\\<\\>\\#\\$\\| \\t]+)>", true);
+		this.m_flagexpr = new ReExt("^([^\\<\\>\\#\\+\\$ \\t]+)", true);
+		this.m_mustflagexpr = new ReExt("^\\$([^\\$\\+\\#\\<\\>]+)", true);
 		this.m_origkey = key;
 		this.__parse(prefix, key, value, isflag);
 	}
@@ -462,8 +477,8 @@ public final class Key {
 		assert(this.m_cmdname != null);
 		this.m_flagname = this.m_cmdname;
 		this.m_cmdname = null;
-		this.m_iscmd = False;
-		this.m_isflag = True;
+		this.m_iscmd = false;
+		this.m_isflag = true;
 		this.__validate();
 		return;
 	}
@@ -483,7 +498,7 @@ public final class Key {
 				}
 			}
 
-			if (this.m_prefix != null && this.m_prefix.length > 0) {
+			if (this.m_prefix != null && this.m_prefix.length() > 0) {
 				retval += String.format("%s-",this.m_prefix);
 			}
 			retval += this.m_flagname;
@@ -496,7 +511,7 @@ public final class Key {
 			}
 		} else if (fldname == "optdest") {
 			retval = "";
-			if (this.m_prefix != null && this.m_prefix.length > 0 ) {
+			if (this.m_prefix != null && this.m_prefix.length() > 0 ) {
 				retval += String.format("%s_",this.m_prefix);
 			}
 			retval += this.m_flagname;
