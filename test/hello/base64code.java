b70ace3afb999a18947c8f41d149327dde5f5df8
jeppeter <jeppeter@gmail.com>
2017-01-05 00:08:14 +0800

    next use base64code

--- test/hello/base64code.java
+++ test/hello/base64code.java
@@ -4,16 +4,34 @@ import ;
 public class base64code {
 
 	static byte[] encode(String input) {
+		
+	}
+
+	static byte[] decode(String input) {
+
+	}
+
+	static String format(byte[] bytes) {
 
 	}
 
 	static void main(String[] args) {
 		int i;
-		if (args.length > 2) {
+		byte[] bytes;
+		String fmtstr;
+		if (args.length >= 2) {
 			if (args[0].equals("encode") == 0) {
-
+				for (i=1;i<args.length;i++) {
+					bytes = encode(args[i]);
+					fmtstr = format(bytes);
+					System.out.printf("%s => %s\n",args[i],fmtstr);
+				}
 			} else if (args[0].equals("decode")==0) {
-
+				for (i=1;i<args.length;i++) {
+					bytes = decode(args[i]);
+					fmtstr = format(bytes);
+					System.out.printf("%s => %s\n",args[i],fmtstr);
+				}
 			} else {
 				System.err.printf("%s not supported\n",args[0]);
 			}
